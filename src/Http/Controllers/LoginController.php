8931ab5a41699f2a470ea51aa6dd79f263ad1ca4
HosseinAlikhani <poshtiban.esteam@gmail.com>
2020-06-27 12:32:55 +0430

    update : add PHPDOc Tag add login() - credentials() - checkAttempt() - setUsername()

--- src/Http/Controllers/LoginController.php
+++ src/Http/Controllers/LoginController.php
@@ -4,21 +4,71 @@ namespace D3CR33\Auth\Http\Controllers;
 
 use D3CR33\Auth\Base\Controllers\BaseController;
 use D3CR33\Auth\Http\Request\LoginRequest;
+use Illuminate\Support\Facades\Auth;
 
+/**
+ * Class LoginController
+ * @package D3CR33\Auth\Http\Controllers
+ */
 class LoginController extends BaseController
 {
+    /**
+     * LoginController constructor.
+     *
+     * @param  LoginRequest  $request
+     */
     public function __construct(LoginRequest $request)
     {
         $this->request = $request;
     }
 
+    /**
+     * return login blade
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function getLogin()
     {
         return view('Auth::login');
     }
 
+    /**
+     * login function for 
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function login()
     {
-        dd($this->request->all());
+        return $this->checkAttempt() ?
+            redirect()->intended('dashboard') :
+            redirect()->route('get.login')->with('message', 'The information entered is incorrect');
+    }
+
+    /**
+     * return username & password
+     * @return array
+     */
+    public function credentials()
+    {
+        return [
+            $this->setUsername() => $this->request->username,
+            'password'           => $this->request->password,
+        ];
+    }
+
+    /**
+     * check user is exists or no
+     * @return bool
+     */
+    public function checkAttempt()
+    {
+        return Auth::attempt($this->credentials());
+    }
+
+    /**
+     * set username
+     * @return string
+     */
+    public function setUsername()
+    {
+        return 'email';
     }
 }
