e022d31466565f3647611e562eceac1f1febf825
HosseinAlikhani <poshtiban.esteam@gmail.com>
2020-06-27 12:31:35 +0430

    add: add LoginRequest.php & RegisterRequest.php

---
+++ src/Http/Request/LoginRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace D3CR33\Auth\Http\Request;
+
+use D3CR33\Auth\Base\Controllers\BaseRequest;
+
+class LoginRequest extends BaseRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
