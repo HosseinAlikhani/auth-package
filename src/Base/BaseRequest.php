98594ea6f0cfaf4a395d72ac54e035a9268d7539
HosseinAlikhani <poshtiban.esteam@gmail.com>
2020-06-27 12:35:03 +0430

    update: add PHPDoc Tag

--- src/Base/BaseRequest.php
+++ src/Base/BaseRequest.php
@@ -7,8 +7,16 @@ use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Validation\ValidationException;
 
+/**
+ * Class BaseRequest
+ * @package D3CR33\Auth\Base\Controllers
+ */
 class BaseRequest extends FormRequest
 {
+    /**
+     * customize return message
+     * @param  Validator  $validator
+     */
     protected function failedValidation(Validator $validator)
     {
         $errors = (new ValidationException($validator))->errors();
