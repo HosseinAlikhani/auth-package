acea84b142533b2f4dbb8663b31a142978807107
HosseinAlikhani <poshtiban.esteam@gmail.com>
2020-06-27 12:28:06 +0430

    update: add PHPDoc Tag

--- src/Auth.php
+++ src/Auth.php
@@ -4,8 +4,18 @@ namespace D3CR33\Auth\Provider;
 use D3CR33\Auth\Routes\RouteRegister;
 use Illuminate\Support\Facades\Route;
 
+/**
+ * Class Auth
+ * @package D3CR33\Auth\Provider
+ * @author Hossein Alikhani
+ */
 class Auth
 {
+    /**
+     * register route to application
+     * @param  null  $callback
+     * @param  array  $options
+     */
     public static function routes($callback = null, array $options = [])
     {
         $callback = $callback ? : function($router) {
