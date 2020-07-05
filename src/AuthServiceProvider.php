7ddc96fdd56be330cbeac1e48c439d85e919e4d4
HosseinAlikhani <poshtiban.esteam@gmail.com>
2020-07-06 01:46:32 +0430

    add: load translations file in AuthServiceProvider.php

--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -13,6 +13,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadViewsFrom(__DIR__ . '/Resources', 'Auth');
+        $this->loadTranslationsFrom(__DIR__ . '/Lang','Auth-Lang');
         $this->loadMigrationsFrom(__DIR__ . '/Database/Migrations');
         $this->publishes([
             __DIR__ . '/Public'    =>  public_path('src'),
