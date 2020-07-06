2c530b05c05166d3c8738d0e349f6387069f8f67
HosseinAlikhani <poshtiban.esteam@gmail.com>
2020-07-06 16:29:10 +0430

    update: update transation content

--- src/Lang/en/trans.php
+++ src/Lang/en/trans.php
@@ -2,12 +2,12 @@
 
 return [
     'LogInToAccount' =>  'Log In To Account',
-    'NewHere'   =>  'New Here',
+    'NewHere'   =>  'New Here ?',
     'CreateAnAccount'   =>  'Create An Account',
     'Username'  =>  'User Name',
     'Password'  =>  'Password',
     'ShowPassword'  =>  'ShowPassword',
     'LogIn' =>  'Log In',
     'KeepMeLoggedIn'    =>  'Keep Me Logged In',
-    'ForgotPassword'    =>  'Forgot Password',
+    'ForgotPassword'    =>  'Forgot Password ?',
 ];
--- src/Lang/fa/trans.php
+++ src/Lang/fa/trans.php
@@ -2,12 +2,12 @@
 
 return [
     'LogInToAccount' =>  'ورود به حساب کاربری',
-    'NewHere'   =>  'تازه آمده اید',
+    'NewHere'   =>  'تازه آمده اید ؟',
     'CreateAnAccount'   =>  'حساب کاربری خود را ایجاد نمایید',
     'Username'  =>  'حساب کاربری',
     'Password'  =>  'رمز عبور',
     'ShowPassword'  =>  'نمایش روز عبور',
     'LogIn' =>  'ورود',
-    'KeepMeLoggedIn'    =>  'مرا وارد شده نگه دار همیشه',
-    'ForgotPassword'    =>  'رمز عبور خود را فراموش کرده اید',
+    'KeepMeLoggedIn'    =>  'من را وارد شده نگه دار همیشه',
+    'ForgotPassword'    =>  'رمز عبور خود را فراموش کرده اید ؟',
 ];
