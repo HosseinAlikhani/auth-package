27c37ad265696821a0fe3da0f3b3a3d5d76bb47c
HosseinAlikhani <poshtiban.esteam@gmail.com>
2020-07-07 00:19:04 +0430

    update: update trans file for register section

--- src/Lang/en/trans.php
+++ src/Lang/en/trans.php
@@ -5,9 +5,21 @@ return [
     'NewHere'   =>  'New Here ?',
     'CreateAnAccount'   =>  'Create An Account',
     'Username'  =>  'User Name',
+    'FirstName' =>  'First Name',
+    'LastName'  =>  'Last Name',
+    'Email' =>  'E-Mail',
     'Password'  =>  'Password',
+    'PasswordConfirmation'  =>  'Password Confirmation',
     'ShowPassword'  =>  'ShowPassword',
     'LogIn' =>  'Log In',
     'KeepMeLoggedIn'    =>  'Keep Me Logged In',
     'ForgotPassword'    =>  'Forgot Password ?',
+    'GetStartWithAFreeAccount'  =>  'Get Start With a Free Account',
+    'AlreadyHaveAnAccount'  =>  'Already Have An Account ?',
+    'IAgreeToThe'   =>  'I Agree To The',
+    'TermsAndConditions'    =>  'Terms And Conditions',
+    'GetStarted'    =>  'Get Started',
+    'message' =>  [
+        'IncorrectData' =>  'The information entered is incorrect',
+    ]
 ];
--- src/Lang/fa/trans.php
+++ src/Lang/fa/trans.php
@@ -5,9 +5,21 @@ return [
     'NewHere'   =>  'تازه آمده اید ؟',
     'CreateAnAccount'   =>  'حساب کاربری خود را ایجاد نمایید',
     'Username'  =>  'حساب کاربری',
+    'FirstName' =>  'نام',
+    'LastName'  =>  'نام خانوادگی',
+    'Email' =>  'پست الکترونیک',
     'Password'  =>  'رمز عبور',
+    'PasswordConfirmation'  =>  'تکرار رمز عبور',
     'ShowPassword'  =>  'نمایش روز عبور',
     'LogIn' =>  'ورود',
     'KeepMeLoggedIn'    =>  'من را وارد شده نگه دار همیشه',
     'ForgotPassword'    =>  'رمز عبور خود را فراموش کرده اید ؟',
+    'GetStartWithAFreeAccount'  =>  'با یک حساب کاربری رایگان شروع کنید',
+    'AlreadyHaveAnAccount'  =>  'قبلاً حساب داشته اید ؟‌',
+    'IAgreeToThe'   =>  'من موافقم',
+    'TermsAndConditions'    =>  'شرایط و ضوابط',
+    'GetStarted'    =>  'شروع کنیم',
+    'message' =>  [
+        'IncorrectData' =>  'اطلاعات وارد شده نادرست است',
+    ]
 ];
