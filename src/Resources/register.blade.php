d790fee9f2dc8893eb610bed397071fb43a7bfb0
HosseinAlikhani <poshtiban.esteam@gmail.com>
2020-07-22 12:30:56 +0430

    update: remove accept with role in register.blade.php

--- src/Resources/register.blade.php
+++ src/Resources/register.blade.php
@@ -46,14 +46,14 @@
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg>
                                 <input id="password_confirmation" name="password_confirmation" type="password" value="" placeholder="{{ __('Auth-Lang::trans.PasswordConfirmation') }}">
                             </div>
-                            <div class="field-wrapper terms_condition">
-                                <div class="n-chk new-checkbox checkbox-outline-primary">
-                                    <label class="new-control new-checkbox checkbox-outline-primary">
-                                        <input type="checkbox" class="new-control-input">
-                                        <span class="new-control-indicator"></span><span> {{ __('Auth-Lang::trans.IAgreeToThe') }} <a href="javascript:void(0);">  {{ __('Auth-Lang::trans.TermsAndConditions') }} </a></span>
-                                    </label>
-                                </div>
-                            </div>
+{{--                            <div class="field-wrapper terms_condition">--}}
+{{--                                <div class="n-chk new-checkbox checkbox-outline-primary">--}}
+{{--                                    <label class="new-control new-checkbox checkbox-outline-primary">--}}
+{{--                                        <input type="checkbox" class="new-control-input">--}}
+{{--                                        <span class="new-control-indicator"></span><span> {{ __('Auth-Lang::trans.IAgreeToThe') }} <a href="javascript:void(0);">  {{ __('Auth-Lang::trans.TermsAndConditions') }} </a></span>--}}
+{{--                                    </label>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
                             <div class="d-sm-flex justify-content-between">
                                 <div class="field-wrapper toggle-pass">
                                     <p class="d-inline-block">{{ __('Auth-Lang::trans.ShowPassword') }}</p>
